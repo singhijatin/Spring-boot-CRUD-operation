ad6177d87bd37b78c635bb50b41449d996e3f606
Jatin Singhi <singhijatin02@gmail.com>
2024-02-01 05:48:28 +0530

    Test 3 added

--- src/main/java/com/boostmytool/beststore/BeststoreApplication.java
+++ src/main/java/com/boostmytool/beststore/BeststoreApplication.java
@@ -9,5 +9,6 @@ public class BeststoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeststoreApplication.class, args);
 	}
-
+	//This is to-do content
+	//Make changes in Local
 }
